f357e64d59ad7813fa4b2f058829e92fc770b1a6
Bunyamin Eren <bunyamin.eren@aarixa.be>
2019-01-29 10:13:40 +0100

    fully functional horizontal play

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,6 +25,7 @@ export class AppComponent {
     if (!this.gameOver && event.code.startsWith("Numpad")) {
       const input = Number(event.code.substr(-1));
       if (input && input % 2 === 0) {
+        // only possible inputs are 2 4 6 8
         this.move(input);
       }
     }
@@ -33,90 +34,65 @@ export class AppComponent {
   move(input: number) {
     let skipNext = false;
     let whileLoop = true;
-    const rows: number[][] = [];
-    const newRows: number[][] = [];
     const possibleNewNumbers = [...Array.from(new Set(this.tiles))].filter(function (el) {
       return el !== null && el !== undefined;
     });
 
-    while (this.tiles.length > 0) {
-      rows.push(this.tiles.splice(0, 4));
-    }
-    switch (input) {
-      case 2:
-        break;
-      case 4:
-        // code for moving everything to the left while adding
-        rows.forEach(row => {
-          row = row.filter(function (el) {
-            return el != null;
-          });
-          const newRow: number[] = [];
-          for (let index = 0; index < row.length; index++) {
-            if (!skipNext) {
-              if (index !== (row.length - 1) && row[index] === row[index + 1]) {
-                newRow.push(row[index] + row[index + 1]);
-                skipNext = true;
-              } else {
-                newRow.push(row[index]);
-              }
-            } else {
-              skipNext = false;
-            }
-          }
-          while (newRow.length < 4) {
-            newRow.push(null);
-          }
-          newRows.push(newRow);
+    if (input === 4 || input === 6) {
+      // handle horizontal move
+      const rows: number[][] = [];
+      const newRows: number[][] = [];
+      while (this.tiles.length > 0) {
+        rows.push(this.tiles.splice(0, 4));
+      }
+      // code for moving everything to the left/right(reverse) while adding
+      rows.forEach(row => {
+        row = row.filter(function (el) {
+          return el != null;
         });
-        // code for generating new tile on the right
-        const rightCol: number[] = [];
-        _.each(newRows, row => {
-          rightCol.push(row[3]);
-        });
-        if (_.indexOf(rightCol, null) > -1) {
-          while (whileLoop) {
-            const randRow: number = Math.floor(Math.random() * 4);
-            if (newRows[randRow][3] === null) {
-              newRows[randRow][3] = _.sample(possibleNewNumbers);
-              whileLoop = false;
-            }
-          }
-        } else {
-          this.gameOver = true;
+        if (input === 6) {
+          row = row.reverse();
         }
-
-        this.tiles = [].concat(...newRows);
-        break;
-      case 6:
-        // code to move everything to the right while adding
-        rows.forEach(row => {
-          row = row.filter(function (el) {
-            return el !== null;
-          }).reverse();
-          const newRow: number[] = [];
-          for (let index = 0; index < row.length; index++) {
-            if (!skipNext) {
-              if (index !== (row.length - 1) && row[index] === row[index + 1]) {
-                newRow.push(row[index] + row[index + 1]);
-                skipNext = true;
-              } else {
-                newRow.push(row[index]);
-              }
+        const newRow: number[] = [];
+        for (let index = 0; index < row.length; index++) {
+          if (!skipNext) {
+            if (index !== (row.length - 1) && row[index] === row[index + 1]) {
+              newRow.push(row[index] + row[index + 1]);
+              skipNext = true;
             } else {
-              skipNext = false;
+              newRow.push(row[index]);
             }
+          } else {
+            skipNext = false;
           }
-          while (newRow.length < 4) {
-            newRow.push(null);
+        }
+        while (newRow.length < 4) {
+          newRow.push(null);
+        }
+        input === 4 ? newRows.push(newRow) : newRows.push(newRow.reverse());
+      });
+      // code for generating new tile on the right
+      const sideCol: number[] = [];
+      // if move left: col is right so index 3, else col is left so index 0
+      const indexToUse: number = input === 4 ? 3 : 0;
+      _.each(newRows, row => {
+        sideCol.push(row[indexToUse]);
+      });
+      if (_.indexOf(sideCol, null) > -1) {
+        while (whileLoop) {
+          const randRow: number = Math.floor(Math.random() * 4);
+          if (newRows[randRow][indexToUse] === null) {
+            newRows[randRow][indexToUse] = _.sample(possibleNewNumbers);
+            whileLoop = false;
           }
-          newRows.push(newRow.reverse());
-        });
+        }
+      } else {
+        this.gameOver = true;
+      }
 
-        this.tiles = [].concat(...newRows);
-        break;
-      case 8:
-        break;
+      this.tiles = [].concat(...newRows);
+    } else {
+      // handle vertical move
     }
     if (this.gameOver) {
       if (confirm("Game over, restart?")) {
